bcce42811c80e79cd85c6ce30837bd27edfbb0cd
lucasfloza <lucasfsouza.l10@gmail.com>
2024-05-17 23:53:22 -0300

    Adding the home section to main

--- js/main.js
+++ js/main.js
@@ -26,3 +26,12 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SHADOW HEADER ===============*/
+
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? Headers.classList.add('shadow-header') : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
