5016f9cc9e2a13fb6e9d8ba34c11f0f619842bbb
lucasfloza <lucasfsouza.l10@gmail.com>
2024-05-23 12:33:48 -0300

    Adding scroll up button

--- js/main.js
+++ js/main.js
@@ -44,3 +44,12 @@ const swiperFavorites = new Swiper('.favorites__swiper', {
     slidesPerView: 'auto',
     centeredSlides: 'auto'
   });
+
+/*================== Scroll Up ==================*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
